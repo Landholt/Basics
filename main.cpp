f861f3b32c14d09226e55173237d067142af9a7f
Joseph <landholtja@gmail.com>
2022-03-03 15:59:19 -0600

    Improved file structure and worked on cpp numerals reference

--- main.cpp
+++ main.cpp
@@ -1,14 +1,12 @@
 //#include <iostream>
-#include "Cpp/test.cpp"
-#include "C/main.c"
 
-extern "C" void assemblyhi();
+#include "includes.h"
 
 int main() {
     std::cout << "\033[2J\033[1;1H";
     cpp::printy();
     printy_c();
-    cpp::integer_help();
+    cpp::numeral_help();
     unsigned short test = 0b1111111111111111;
     short test1 = 0b1111111111111111;
     std::cout << test << std::endl << test1 << std::endl;
